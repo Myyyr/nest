23309e60413ecb189934b15597b93923abcfeb57
Myyyr <loic.themyr@gmail.com>
2021-07-13 11:56:24 +0200

    debug

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=2.5e-4, type=float, help='learning rate')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
 args = parser.parse_args()
