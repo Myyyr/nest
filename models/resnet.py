1d50418b284818941c7a866f23c3c9c14b3a4327
Myyyr <loic.themyr@gmail.com>
2021-07-15 16:51:54 +0200

    debug

--- models/resnet.py
+++ models/resnet.py
@@ -101,8 +101,6 @@ class ResNet(nn.Module):
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        print(out.shape)
-        exit(0)
         return out
 
 
